9f76bee385ef0a7e3eedc0ded45dd76d8394072c
Gunter25 <56507883+Gunter25@users.noreply.github.com>
2021-02-10 22:35:21 +0000

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,25 +1,3 @@
-resource "aws_security_group" "web-node" {
-  name = "web-node"
-  description = "Web Security Group"
-  ingress {
-    from_port = 80
-    to_port = 80
-    protocol = "ssh"
-   
-  }
-  ingress {
-    from_port = 22
-    to_port = 22
-    protocol = "ssh"
-   
-  }    
-  egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
-   
-  }
-}
 resource "aws_instance" "demo" {
   ami = lookup(var.AMI, var.REGION)
   instance_type = var.INSTANCE_TYPE
@@ -31,5 +9,28 @@ resource "aws_instance" "demo" {
   count = var.COUNT
   allow {
     protocol = "ssh"
+	ports    = ["80", "22"]
+  }
+}
+resource "aws_security_group" "web-node" {
+  name = "web-node"
+  description = "Web Security Group"
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }    
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
