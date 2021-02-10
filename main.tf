a43c0fa96a1c1f2670533855a1c952561d019cbf
Gunter25 <56507883+Gunter25@users.noreply.github.com>
2021-02-10 22:29:20 +0000

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,31 +1,32 @@
-resource "aws_security_group" "allow_tls" {
-  name        = "allow_tls"
-  description = "Allow TLS inbound traffic"
-  vpc_id      = aws_vpc.main.id
-
+resource "aws_security_group" "web-node" {
+  name = "web-node"
+  description = "Web Security Group"
   ingress {
-    description = "TLS from VPC"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "ssh"
-    cidr_blocks = [aws_vpc.main.cidr_block]
+    from_port = 80
+    to_port = 80
+    protocol = "ssh"
+   
   }
-
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "ssh"
+   
+  }    
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+   
   }
 }
 resource "aws_instance" "demo" {
   ami = lookup(var.AMI, var.REGION)
   instance_type = var.INSTANCE_TYPE
   key_name = var.KEY_NAME
-  security_groups = ["${aws_security_group.allow_tls.name}"]
+  security_groups = ["${aws_security_group.web-node.name}"]
   tags = {
 	Name = var.TAGS
-	Name = "allow_tls"
   }
   count = var.COUNT
   allow {
