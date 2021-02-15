b00b78ffdb4362d291691c5eb2a0dc017d3607ae
Gunter25 <56507883+Gunter25@users.noreply.github.com>
2021-02-15 21:51:33 +0000

    Add files via upload

--- main.tf
+++ main.tf
@@ -2,19 +2,28 @@ resource "aws_instance" "demo" {
   ami = lookup(var.AMI, var.REGION)
   instance_type = var.INSTANCE_TYPE
   key_name = var.KEY_NAME
-  vpc_security_group_ids = [aws_security_group.instance.id]
+  vpc_security_group_ids = [aws_security_group.web-sg.id]
   tags = {
 	Name = var.TAGS
   }
   count = var.COUNT
 }
 
-resource "aws_security_group" "instance" {
-  name = "terraform-example-instance"
+resource "aws_security_group" "web-sg" {
+  name = "${random_pet.name.id}-sg"
   ingress {
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
+
+
