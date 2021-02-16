14429c62357109a4c2b01c087dde4e459cabaa87
Gunter25 <56507883+Gunter25@users.noreply.github.com>
2021-02-16 08:20:17 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ resource "aws_security_group" "demo" {
   ingress {
     from_port   = 80
     to_port     = 80
-    protocol    = "ssh"
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
